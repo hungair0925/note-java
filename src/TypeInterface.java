b2030202c324f6173dcc4c8b5b968609b1da1f3e
hungair0925 <hungair0925@gmail.com>
2023-03-29 00:22:38 +0900

    型推論

---
+++ src/TypeInterface.java
@@ -0,0 +1,11 @@
+public class TypeInterface {
+    public static void main(String[] args) {
+        var name = "ねこぐらまー";
+        var weight =  15;
+        var isMale = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Weight: " + weight + "[kg]");
+        System.out.println("Male: " + isMale);
+    }
+}
