6c08e50000dacae497f425f9a6575839d1cf4ea1
hungair0925 <hungair0925@gmail.com>
2023-03-29 00:10:56 +0900

    基本データ型

---
+++ src/PrimitiveType.java
@@ -0,0 +1,21 @@
+public class PrimitiveType {
+    public static void main(String[] args) {
+        int num = 123;
+        long longNum = 1234567890L;
+        double decimal = 3.14;
+        float floatDecimal = 3.14159236F;
+
+        char character = '猫';
+
+        boolean isCat = true;
+
+        System.out.println("num=" + num);
+        System.out.println("longNum=" + longNum);
+        System.out.println("decimal=" + decimal);
+        System.out.println("floatDecimal=" + floatDecimal);
+
+        System.out.println("character=" + character);
+
+        System.out.println("boolean=" + isCat);
+    }
+}
