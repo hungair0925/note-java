6f0d15d98bac2a233a57b15e1b7dbee2c6953e85
hungair0925 <hungair0925@gmail.com>
2023-03-28 23:49:21 +0900

    変数宣言

---
+++ src/VariableDeclaration.java
@@ -0,0 +1,10 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int old, height;
+        old = 20;
+        height = 175;
+
+        System.out.println("年齢:" + old);
+        System.out.println("身長:" + height);
+    }
+}
